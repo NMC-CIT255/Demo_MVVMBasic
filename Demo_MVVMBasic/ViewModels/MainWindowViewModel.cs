196970843e901fa94e7be8437df84ca6e4fa1e0f
John Velis <john@johnvelis.com>
2020-09-03 15:44:08 -0400

    Add project files.

---
+++ Demo_MVVMBasic/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+using System.Windows;
+
+using Demo_MVVMBasic.Models;
+using Demo_MVVMBasic.Data;
+using System.Windows.Controls;
+
+namespace Demo_MVVMBasic
+{
+    class MainWindowViewModel
+    {
+        public ICommand ButtonSellCommand { get; set; }
+
+        public List<Widget> Widgets { get; set; }
+        public Widget SelectedWidget { get; set; }
+
+        public MainWindowViewModel()
+        {
+            Widgets = WidgetData.GetAllWidgets();
+            if (Widgets.Any()) SelectedWidget = Widgets[0];
+
+            ButtonSellCommand = new RelayCommand(new Action<object>(SellWidgets));
+        }
+
+        public void SellWidgets(object parameter)
+        {
+            int.TryParse((string)parameter, out int quantity);
+            SelectedWidget.CurrentInventory -= quantity;
+        }
+    }
+}
